82f4f53b3dd290d9f2695a061253cb0f47506787
blakemecham <bam.mecham@gmail.com>
2018-09-17 13:36:43 -0600

    icons from font awesome

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import Icon from '../icon';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className='library-course'>
                 <lable className='library-course__title'>Problem Solving</lable>
+                {Icon('fas fa-check', 'library-course__icon')}
+                {Icon('fas fa-plus-circle', 'library-course__action')}
                 <div className='library-course__description'>
                     <lable>Course Description</lable>
                     <p>This is awesome!</p>
